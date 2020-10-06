ba394a92e80517ebf9015565ff964bece4f976f7
ibon-oi <60422906+ibon-oi@users.noreply.github.com>
2020-10-07 01:13:27 +0200

    fracciones

---
+++ Tarea21/fracciones.py
@@ -0,0 +1,21 @@
+#funciona tanto con números con . o , (0.5 o 0,5)
+print ("Introduce un número entre 0,0001 y 0,9999")
+num = (input())
+
+num = num.replace(",",".")
+num = float(num)
+print(f"El número introducido es: {num}")
+
+den = 10000
+num = int(num * 10000)
+i = num
+count = 0
+while i > 1:
+    # Si el numerador y el denominador son divisibles entre i, se asignan nuevos denominador y numerador
+    if num % i == 0 and den % i == 0:
+        num = num / i
+        den = den / i
+    i = i - 1
+    count = count + 1
+
+print("Resultado: %d/%d. Iteraciones realizadas: %d" % (num, den, count))

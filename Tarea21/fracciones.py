abf5c5e47b40b81b0f5aa7e00d7ab101b094b3e5
ibon-oi <60422906+ibon-oi@users.noreply.github.com>
2020-10-07 01:15:53 +0200

    comments

--- Tarea21/fracciones.py
+++ Tarea21/fracciones.py
@@ -1,8 +1,9 @@
 #funciona tanto con números con . o , (0.5 o 0,5)
 print ("Introduce un número entre 0,0001 y 0,9999")
 num = (input())
-
+# obtenemos un string para poder cambiar la , por .
 num = num.replace(",",".")
+# conertimos el string eln float para poder interactuar
 num = float(num)
 print(f"El número introducido es: {num}")
 
